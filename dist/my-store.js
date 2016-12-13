e273002cd48468edb71a7b28163fa8a0d6619abf
Holger Schauf <fluse@fluse.fritz.box>
2016-12-13 16:04:59 +0100

    compiling

--- dist/my-store.js
+++ dist/my-store.js
@@ -16,19 +16,26 @@ var _extend = require('extend');
 
 var _extend2 = _interopRequireDefault(_extend);
 
+var _cryptoJs = require('crypto-js');
+
+var _cryptoJs2 = _interopRequireDefault(_cryptoJs);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Storage = function () {
     function Storage() {
+        var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
         _classCallCheck(this, Storage);
 
         this.isClient = typeof window !== 'undefined';
 
-        this.defaultOptions = {
-            expiration: 24
-        };
+        this.defaultOptions = (0, _extend2.default)({
+            expiration: 24,
+            secret: 'myStore'
+        }, options);
     }
 
     _createClass(Storage, [{
@@ -49,7 +56,9 @@ var Storage = function () {
             };
 
             try {
-                localStorage.setItem(name, JSON.stringify(store));
+                var stringified = JSON.stringify(store);
+                var encrypedStore = _cryptoJs2.default.AES.encrypt(stringified, options.secret);
+                localStorage.setItem(name, encrypedStore);
             } catch (e) {}
         }
     }, {
@@ -58,11 +67,14 @@ var Storage = function () {
             var _this = this;
 
             var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+            var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
 
 
             if (!this.isClient) {
                 return null;
             }
+
+            options = (0, _extend2.default)({}, this.defaultOptions, options);
 
             try {
                 var _ret = function () {
@@ -74,7 +86,9 @@ var Storage = function () {
                         };
                     }
 
-                    store = JSON.parse(store);
+                    var encrypedStore = _cryptoJs2.default.AES.decrypt(ciphertext.toString(), options.secret);
+
+                    store = JSON.parse(encrypedStore);
                     var now = (0, _moment2.default)();
 
                     if ((0, _moment2.default)(store.expiredAt).isBefore(now)) {
